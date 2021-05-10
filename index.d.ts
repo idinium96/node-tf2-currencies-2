8d05d57b047ac6e808732166919f0f705baedad2
Preport <erdemgezzz@gmail.com>
2021-05-10 15:38:06 +0300

    Fix Type Declaration

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,24 @@
-declare module 'tf2-currencies-2' {
-    class Currencies {
-        static toCurrencies(value: number, conversion?: number): Currencies;
+export default class Currencies {
+    static toCurrencies(value: number, conversion?: number): Currencies;
 
-        static toRefined(scrap: number): number;
+    static toRefined(scrap: number): number;
 
-        static toScrap(refined: number): number;
+    static toScrap(refined: number): number;
 
-        static addRefined(...arg: number[]): number;
+    static addRefined(...arg: number[]): number;
 
-        constructor(currencies: { keys?: number; metal?: number });
+    constructor(currencies: { keys?: number; metal?: number });
 
+    keys: number;
+
+    metal: number;
+
+    toValue(conversion?: number): number;
+
+    toString(): string;
+
+    toJSON(): {
         keys: number;
-
         metal: number;
-
-        toValue(conversion?: number): number;
-
-        toString(): string;
-
-        toJSON(): {
-            keys: number;
-            metal: number;
-        };
-    }
-
-    export = Currencies;
+    };
 }
